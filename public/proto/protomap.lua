1f64880abc2b2a6b9ac2195a18c9eb313c5666fb
Gowa2017 <33367355+Gowa2017@users.noreply.github.com>
2021-10-29 14:55:36 +0800

    modif proto

--- public/proto/protomap.lua
+++ public/proto/protomap.lua
@@ -1,24 +1,24 @@
 return {
   c2s     = {
-    ["c2s.login.Auth"] = 20002,
+    ["c2s.game.Logon"] = 20004,
+    ["c2s.login.ChallengeResp"] = 20002,
     ["c2s.login.Handshake"] = 20003,
     ["c2s.login.Login"] = 20001,
   },
   c2sbyid = {
+    [20004] = "c2s.game.Logon",
     [20001] = "c2s.login.Login",
-    [20002] = "c2s.login.Auth",
+    [20002] = "c2s.login.ChallengeResp",
     [20003] = "c2s.login.Handshake",
   },
   s2c     = {
     ["s2c.login.Challenge"] = 10001,
-    ["s2c.login.HandshakeOK"] = 10003,
-    ["s2c.login.Key"] = 10004,
-    ["s2c.login.LoginOK"] = 10002,
+    ["s2c.login.Key"] = 10003,
+    ["s2c.login.Resp"] = 10002,
   },
   s2cbyid = {
-    [10004] = "s2c.login.Key",
     [10001] = "s2c.login.Challenge",
-    [10002] = "s2c.login.LoginOK",
-    [10003] = "s2c.login.HandshakeOK",
+    [10002] = "s2c.login.Resp",
+    [10003] = "s2c.login.Key",
   },
 }
