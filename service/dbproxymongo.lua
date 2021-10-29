5586270967bb6df93a014ce4eec63c52e404ccaf
Gowa2017 <33367355+Gowa2017@users.noreply.github.com>
2021-10-29 14:56:17 +0800

    reformat it

--- service/dbproxymongo.lua
+++ service/dbproxymongo.lua
@@ -5,11 +5,16 @@ local mongo   = require("skynet.db.mongo")
 
 local db     
 local CMD     = {}
-function CMD.start(conf) db = mongo.client(conf):getDB(conf.authdb) end
+function CMD.start(conf)
+  print("connect db")
+  db = mongo.client(conf):getDB(conf.authdb)
+end
 
 local ACTION  = {}
 
-function ACTION.insert(t, doc) return db:getCollection(t):safe_insert(doc) end
+function ACTION.insert(t, doc)
+  return db:getCollection(t):safe_insert(doc)
+end
 
 function ACTION.update(t, condtion, value)
   return db:getCollection(t):safe_update(condtion, { ["$set"] = value })
@@ -22,9 +27,13 @@ function ACTION.find(t, q, s)
   return r
 end
 
-function ACTION.findOne(t, q, s) return db:getCollection(t):findOne(q, s) end
+function ACTION.findOne(t, q, s)
+  return db:getCollection(t):findOne(q, s)
+end
 
-function ACTION.delete(t, s) return db:getCollection(t):safe_delete(s) end
+function ACTION.delete(t, s)
+  return db:getCollection(t):safe_delete(s)
+end
 
 service.setMessageCmds("lua", CMD)
 service.enableMessage("db")
