7f5ba92f8f3b46fba1f527f645c8b1b7e32dc556
Gowa2017 <33367355+Gowa2017@users.noreply.github.com>
2021-10-29 16:53:24 +0800

    update message map

--- public/proto/protomap.lua
+++ public/proto/protomap.lua
@@ -1,22 +1,26 @@
 return {
   c2s     = {
+    ["c2s.game.Enter"] = 20005,
     ["c2s.game.Logon"] = 20004,
     ["c2s.login.ChallengeResp"] = 20002,
     ["c2s.login.Handshake"] = 20003,
     ["c2s.login.Login"] = 20001,
   },
   c2sbyid = {
-    [20004] = "c2s.game.Logon",
     [20001] = "c2s.login.Login",
     [20002] = "c2s.login.ChallengeResp",
     [20003] = "c2s.login.Handshake",
+    [20004] = "c2s.game.Logon",
+    [20005] = "c2s.game.Enter",
   },
   s2c     = {
+    ["s2c.game.Scene"] = 10004,
     ["s2c.login.Challenge"] = 10001,
     ["s2c.login.Key"] = 10003,
     ["s2c.login.Resp"] = 10002,
   },
   s2cbyid = {
+    [10004] = "s2c.game.Scene",
     [10001] = "s2c.login.Challenge",
     [10002] = "s2c.login.Resp",
     [10003] = "s2c.login.Key",
