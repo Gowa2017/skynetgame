8c4d8dc5a27172bb1e98534ba8faca1ad6c601eb
Gowa2017 <33367355+Gowa2017@users.noreply.github.com>
2021-10-29 16:53:38 +0800

    start world and agentpool service

--- service/start.lua
+++ service/start.lua
@@ -1,38 +1,23 @@
 local skynet     = require("skynet")
 require "skynet.manager"
 local sharetable = require("skynet.sharetable")
-local cluster    = require("skynet.cluster")
 
 skynet.start(function()
-  local mode = skynet.getenv "mode"
-  if mode then cluster.open(mode) end
-  if mode == "G" then
-    sharetable.loadfile(skynet.getenv("daobiao"))
-
-    skynet.newservice("debug_console", 7001)
-    local loginserver = cluster.proxy("L@logind")
-    local gate        = skynet.newservice("gated", loginserver)
-    cluster.register("sample", gate)
-    skynet.call(gate, "lua", "open",
-                { port       = 8888, maxclient  = 64, servername = "sample" })
-    local t           = skynet.time()
-    skynet.newservice("agentpool")
-    print(skynet.time() - t)
-
-  elseif mode == "L" then
-    skynet.newservice("debug_console", 7002)
-
-    local loginserver = skynet.newservice("logind")
-    cluster.register("logind", loginserver)
-
-    local db          = skynet.newservice("dbproxymongo")
-    skynet.name(".accdb", db)
-    skynet.call(db, "lua", "start", {
-      host     = "127.0.0.1",
-      port     = 27017,
-      username = "acc",
-      password = "wouinibaba",
-      authdb   = "account",
-    })
-  end
+  sharetable.loadfile(skynet.getenv("daobiao"))
+  skynet.newservice("debug_console", 7001)
+  skynet.newservice("agentpool")
+  skynet.newservice("world")
+  local login = skynet.newservice("logind")
+  local gate  = skynet.newservice("gated", login)
+  skynet.call(gate, "lua", "open",
+              { port       = 8888, maxclient  = 64, servername = "sample" })
+  local db    = skynet.newservice("dbproxymongo")
+  skynet.name(".accdb", db)
+  skynet.call(db, "lua", "start", {
+    host     = "127.0.0.1",
+    port     = 27017,
+    username = "acc",
+    password = "wouinibaba",
+    authdb   = "account",
+  })
 end)
