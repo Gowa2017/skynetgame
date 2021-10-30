7970c2c7c17ae062da0a9ba505612f77af84617c
Gowa2017 <33367355+Gowa2017@users.noreply.github.com>
2021-10-30 22:29:43 +0800

    save proto id to full message name

--- public/proto/gen.lua
+++ public/proto/gen.lua
@@ -50,33 +50,6 @@ local function genProtoClass()
   os.execute("lua-format -i " .. protoClassFile)
 end
 
----To get a field like  a.b.c.d.e.f from a table
----@param T table
----@param k string a.b.c.d.e.f
-local function getfield(T, k)
-  local v = T -- start with the table of globals
-  for w in string.gmatch(k, "[%a_][%w_]*") do
-    v = v[w]
-    if not v then return v end
-  end
-  return v
-end
----To set a field like  a.b.c.d.e.f on a table
----@param T table
----@param k string a.b.d.c
----@param v any
-local function setfield(T, k, v)
-  local base = T
-  for w, d in string.gmatch(k, "([%a_][%w_]*)(%.?)") do
-    if d == "." then -- not last item
-      base[w] = base[w] or {}
-      base = base[w]
-    else
-      base[w] = v
-    end
-  end
-end
-
 local function genProtoMap()
   local ok, err = loadfile(protoMapFile, "bt")
   local def     = ok and ok() or
@@ -101,7 +74,7 @@ local function genProtoMap()
       local id = seq[ttype] + 1
       seq[ttype] = id
       def[ttype][name:sub(2)] = id
-      def[byid][id] = name:sub(2)
+      def[byid][id] = { ttype, grp, message }
     end
   end
 
