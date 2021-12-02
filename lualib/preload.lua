c1b224b3c09dfa462ea805d0d24839ae31bfb21a
Gowa2017 <33367355+Gowa2017@users.noreply.github.com>
2021-12-02 20:37:59 +0800

    preload logmodule

--- lualib/preload.lua
+++ lualib/preload.lua
@@ -1,6 +1,7 @@
 SERVICE_NAME = SERVICE_NAME or ...
 --- user for logger
 SERVICE_DESC = table.concat({ ... })
+LOG = require("go.logger")
 
 local skynet = require("skynet")
 if skynet.getenv "dev" then
