3c069bf0ad69615b8f25e5ce8371c5ff47351d73
Gowa2017 <33367355+Gowa2017@users.noreply.github.com>
2021-12-29 16:28:00 +0800

    add start

--- service/start.lua
+++ service/start.lua
@@ -1,28 +1,24 @@
 local skynet     = require("skynet")
 require "skynet.manager"
 local sharetable = require("skynet.sharetable")
+local dbproxy    = require("dbproxy")
 
 skynet.start(function()
   sharetable.loadfile(skynet.getenv("daobiao"))
   skynet.newservice("debug_console", 7001)
   skynet.newservice("agentpool")
-  skynet.newservice("world")
-  local login   = skynet.newservice("logind")
-  local gate    = skynet.newservice("gated", login)
+  local login    = skynet.newservice("logind")
+  local gate     = skynet.newservice("gated", login)
   skynet.call(gate, "lua", "open",
               { port       = 8888, maxclient  = 64, servername = "sample" })
-  local db      = skynet.newservice("dbproxymongo")
-  skynet.name(".accdb", db)
-  skynet.call(db, "lua", "start", {
-    host     = "42.192.43.15",
-    port     = 27017,
-    username = "acc",
-    password = "wouinibaba",
-    authdb   = "account",
-  })
-  local message = require("conf.message")
-  skynet.register_protocol(message.db)
-  local r       = skynet.call(db, "db", "find", "account", { id = 1 })
-  local pretty  = require("pl.pretty")
-  pretty.dump(r)
+
+  local my       = skynet.newservice("testmysqldb")
+  local mongo    = skynet.newservice("testmongodb")
+
+  local climy    = dbproxy.wrap(my)
+  local climongo = dbproxy.wrap(mongo)
+
+  climongo:run("register", "user", "test")
+  climy:run("register", "user", "test")
+
 end)
