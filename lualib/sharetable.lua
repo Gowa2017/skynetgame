b6a7d13f5dc464bd491a7aa3585435bb3f32a686
Gowa2017 <33367355+Gowa2017@users.noreply.github.com>
2021-12-31 17:39:21 +0800

    add sharetable wrapper

---
+++ lualib/sharetable.lua
@@ -0,0 +1,22 @@
+local sharetable = require("skynet.sharetable")
+
+local _mt        = {
+  __index = function(self, key)
+    if self.version < self.box.version then
+      self.box = sharetable.query(self.filename)
+    end
+    return self.box[key]
+  end,
+}
+
+local M          = {}
+function M.share(filename)
+  sharetable.loadfile(filename)
+end
+function M.query(filename)
+  local t = sharetable.query(filename)
+  if not t then
+    error(string.format("No this sharetable %s", filename))
+  end
+  return setmetatable({ filename = filename, box      = t }, _mt)
+end
