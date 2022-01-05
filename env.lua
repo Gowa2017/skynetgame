749cd8781ba88c7808cf31795c9539dbc70e4f94
Gowa2017 <33367355+Gowa2017@users.noreply.github.com>
2022-01-05 09:32:25 +0800

    add pl to test env

--- env.lua
+++ env.lua
@@ -2,3 +2,6 @@ local config = {}
 loadfile("./config.path.lua", "bt", config)()
 package.path = package.path .. ";" .. config.lua_path
 package.cpath = package.cpath .. ";" .. config.lua_cpath
+package.path = package.path .. ";3rd/Penlight/lua/?.lua"
+
+CLASS = require("class")
